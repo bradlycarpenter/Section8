5a272c87de0ff043c215fdf5b6bcdfa3dabd7c3d
bradlycarpenter <bradlycarpenterza@gmail.com>
2024-05-10 15:54:44 +0200

    PC 10/05/2024

--- Section8_TestingForEquality/Section8_TestingForEquality.cpp
+++ Section8_TestingForEquality/Section8_TestingForEquality.cpp
@@ -1,11 +1,12 @@
-# include <iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     bool equal_result{false};
     bool not_equal_result{false};
 
-    int num1{},num2{};
+    int num1{}, num2{};
 
     cout << boolalpha;
 
@@ -15,25 +16,25 @@ int main(){
     // equal_result = (num1 == num2);
     // not_equal_result = (num1 != num2);
     // cout << "Comparison result (equals): "  << equal_result << endl;
-    // cout << "Comparison result (not_equals): " << not_equal_result << endl; 
+    // cout << "Comparison result (not_equals): " << not_equal_result << endl;
 
-    //Characters
-    // cout << "Enter two characters seperated by a space: ";
-    // char char1{}, char2{};
-    // cin >> char1 >> char2;
-    // equal_result = (char1 == char2);
-    // not_equal_result = (char1 != char2);
-    // cout << "Comparison result (equals): "  << equal_result << endl;
-    // cout << "Comparison result (not_equals): " << not_equal_result << endl; 
+    // Characters
+    //  cout << "Enter two characters seperated by a space: ";
+    //  char char1{}, char2{};
+    //  cin >> char1 >> char2;
+    //  equal_result = (char1 == char2);
+    //  not_equal_result = (char1 != char2);
+    //  cout << "Comparison result (equals): "  << equal_result << endl;
+    //  cout << "Comparison result (not_equals): " << not_equal_result << endl;
 
-    //Doubles
-    // cout << "Enter two doubles seperated by a space: ";
-    // double double1{}, double2{};
-    // cin >> double1 >> double2;
-    // equal_result = (double1 == double2);
-    // not_equal_result = (double1 != double2);
-    // cout << "Comparison result (equals): "  << equal_result << endl;
-    // cout << "Comparison result (not_equals): " << not_equal_result << endl; 
+    // Doubles
+    //  cout << "Enter two doubles seperated by a space: ";
+    //  double double1{}, double2{};
+    //  cin >> double1 >> double2;
+    //  equal_result = (double1 == double2);
+    //  not_equal_result = (double1 != double2);
+    //  cout << "Comparison result (equals): "  << equal_result << endl;
+    //  cout << "Comparison result (not_equals): " << not_equal_result << endl;
 
     // Integer and Doubke
     cout << "Enter an integer and a double seperated by a space: ";
@@ -41,6 +42,6 @@ int main(){
     cin >> num1 >> double1;
     equal_result = (num1 == double1);
     not_equal_result = (num1 != double1);
-    cout << "Comparison result (equals): "  << equal_result << endl;
-    cout << "Comparison result (not_equals): " << not_equal_result << endl; 
+    cout << "Comparison result (equals): " << equal_result << endl;
+    cout << "Comparison result (not_equals): " << not_equal_result << endl;
 }
