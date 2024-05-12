5b72f6e29cdf862200064e5c48ca05692d9be6fa
bradlycarpenter <bradlycarpenterza@gmail.com>
2024-05-12 23:02:37 +0200

    Section 8 Exercise

---
+++ Section 8 Exercise/Section 8 Exercise.cpp
@@ -0,0 +1,22 @@
+# include <iostream>
+
+using namespace std;
+
+int main(int age, bool parental_consent, bool ssn, bool accidents) {
+    
+    //----WRITE YOUR CODE BELOW THIS LINE----
+    age = 19;
+    parental_consent = true;
+    ssn = true;
+    accidents = false;
+
+
+     if ( (age >= 18 || (age >= 16 && parental_consent)) && ssn && !accidents){
+        //WRITE ALL YOUR CODE WITHIN THE PARENTHESES
+         cout << "Yes, you can work.";
+     } else {
+        cout << "No, you can not work.";
+     }
+    // //----WRITE YOUR CODE ABOVE THIS LINE----
+   
+}
